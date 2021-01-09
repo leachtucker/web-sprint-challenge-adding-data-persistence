152de156dd514919e7e2593772eb2b721de84ede
Tucker <leachtucker@gmail.com>
2021-01-08 17:21:24 -0800

    Built DB helper functions for tasks-model

--- api/tasks/model.js
+++ api/tasks/model.js
@@ -1,1 +1,14 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+async function getTasks() {
+    return await db('tasks');
+}
+
+async function insertTask(task) {
+    return await db('tasks').insert(task);
+}
+
+module.exports = {
+    getTasks,
+    insertTask
+}
