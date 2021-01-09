e275a0b796f4eea55c8e8e324fce247734d76012
Tucker <leachtucker@gmail.com>
2021-01-08 17:20:31 -0800

    Built DB helper functions for projects-model

--- api/projects/model.js
+++ api/projects/model.js
@@ -1,1 +1,14 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+async function getProjects() {
+    return await db('projects');
+}
+
+async function insertProject(project) {
+    return await db('projects').insert(project);
+}
+
+module.exports = {
+    getProjects,
+    insertProject
+}
