9aa8d1f81883daea4a70fa8f00e48f03a5d1276d
Tucker <leachtucker@gmail.com>
2021-01-08 17:18:22 -0800

    Built DB helper functions for resources-model

--- api/resources/model.js
+++ api/resources/model.js
@@ -1,1 +1,14 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+async function getResources() {
+    return await db('resources');
+}
+
+async function insertResource(resource) {
+    return await db('resources').insert(resource);
+}
+
+module.exports = {
+    getResources,
+    insertResource
+};
